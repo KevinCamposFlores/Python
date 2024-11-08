fadfc4605e5c9ef43598d6f0946e10ed5ca404c7
kevinCmpFlores <kevincfj29@gmail.com>
2024-11-08 05:21:01 -0500

    primer commit

---
+++ python.py
@@ -0,0 +1,15 @@
+print("Hola Mundo")
+#hola perros
+
+print("resultado" , str(6))
+
+print((not(7 != 7) and 6>5) and ('rozar' <'rotar' or not(3==5)))
+
+
+string_bb = 'stri'
+
+a,g,v,f = string_bb
+print(a)
+print(g)
+print(v)
+print(f)
